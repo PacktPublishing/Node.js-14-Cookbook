ce759b45a7cdc0fd9bab421bbc950cf8e6af2134
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2020-05-19 13:30:10 +0100

    Chapter 2: file watching change

--- Chapter02/file-watching/watch.js
+++ Chapter02/file-watching/watch.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const file = "./file.txt";
+const moment = require("moment");
 
-fs.watchFile(file, (current, previous) => {
-  return console.log(`${file} updated ${time}`);
+fs.watch(file, (eventType, filename) => {
+  const time = moment().format("MMMM Do YYYY, h:mm:ss a");
+  return console.log(`${filename} updated ${time}`);
 });
--- Chapter02/file-watching/watch2.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const file = "./file.txt";
-const moment = require("moment");
-
-fs.watch(file, (eventType, filename) => {
-  const time = moment().format("MMMM Do YYYY, h:mm:ss a");
-  return console.log(`${filename} updated ${time}`);
-});
