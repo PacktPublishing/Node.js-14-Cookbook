4f825bdac3c21386924c6363fbab746f4555b267
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2020-05-22 11:49:31 +0100

    Chapter 6: updates

--- Chapter06/express-app/index.js
+++ Chapter06/express-app/index.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const path = require('path')
-const index = require('./routes/index')
+const express = require("express");
+const path = require("path");
+const index = require("./routes/index");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', index)
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", index);
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+});
