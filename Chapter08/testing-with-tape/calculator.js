a900e9de68c333f6205fc9beaf55c4c42735a64e
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2020-07-01 03:42:34 +0100

    Chapter 8: verify testing-with-tape

--- Chapter08/testing-with-tape/calculator.js
+++ Chapter08/testing-with-tape/calculator.js
@@ -1,15 +1,16 @@
 module.exports.add = (number1, number2) => {
-  return number1 + number2;
+    return number1 + number2;
 };
 
 module.exports.subtract = (number1, number2) => {
-  return number1 - number2;
+    return number1 - number2;
 };
 
 module.exports.multiply = (number1, number2) => {
-  return number1 * number2;
+    return number1 * number2;
 };
 
 module.exports.divide = (number1, number2) => {
-  return number1 / number2;
+    return number1 / number2;
 };
+
