d74bfe94dc1350fa27d0d650d93b3d32ce8eda3e
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2020-08-14 13:34:15 +0100

    Chapter 10: Benchmarking recipe

---
+++ Chapter10/benchmarking-http/post-server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/", (req, res) => {
+  res.send(req.body);
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
+
