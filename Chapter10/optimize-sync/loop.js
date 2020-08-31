45b0a0c74ec46ed51f7fe25334b2fce7e455dc87
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2020-08-31 01:38:20 +0100

    Chapter 10: Optimize Async samples

--- Chapter10/optimize-sync/loop.js
+++ Chapter10/optimize-sync/loop.js
@@ -1,10 +1,10 @@
 function sumOfSquares(maxNumber) {
-    let i = 0;
-    let sum = 0;
-    for (i; i <= maxNumber; i++) {
-        sum += i ** 2;
-    }
-    return sum;
+  let i = 0;
+  let sum = 0;
+  for (i; i <= maxNumber; i++) {
+    sum += i ** 2;
+  }
+  return sum;
 }
 
 module.exports = sumOfSquares;
