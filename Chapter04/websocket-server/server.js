5b5fe1715b1d5cb3ca3dd156b05724c0c248f576
Wolfgang-stack <wolfgang.a.mcrae@gmail.com>
2021-11-21 11:29:16 -0800

    Updated Chapter04: WebSocket Server
    version 8.0.0 introduced breaking changes https://github.com/websockets/ws/releases/tag/8.0.0
    
    Text messages will no longer be decoded to strings and will be passed as buffers

--- Chapter04/websocket-server/server.js
+++ Chapter04/websocket-server/server.js
@@ -1,12 +1,15 @@
-const WebSocket = require("ws");
+const WebSocket = require('ws');
 
 const WebSocketServer = new WebSocket.Server({
-  port: 3000,
+  port: 3000
 });
 
-WebSocketServer.on("connection", (socket) => {
-  socket.on("message", (msg) => {
-    console.log("Received:", msg);
-    if (msg === "Hello") socket.send("World!");
+WebSocketServer.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    let msgString = msg.toString();
+
+    console.log('Received:', msgString);
+
+    if (msgString === 'Hello') socket.send('World!');
   });
 });
