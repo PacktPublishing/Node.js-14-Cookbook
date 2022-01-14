fe8501856ee3d9dd044d4b59eff74bd645103326
Luis_Munoz <lmemunoz@gmail.com>
2022-01-15 00:46:27 +1100

    fix received message
    fix received message

--- Chapter04/websocket-server/server.js
+++ Chapter04/websocket-server/server.js
@@ -6,7 +6,7 @@ const WebSocketServer = new WebSocket.Server({
 
 WebSocketServer.on("connection", (socket) => {
   socket.on("message", (msg) => {
-    console.log("Received:", msg);
-    if (msg === "Hello") socket.send("World!");
+    console.log("Received:", msg.toString());
+    if (msg.toString() === "Hello") socket.send("World!");
   });
 });
